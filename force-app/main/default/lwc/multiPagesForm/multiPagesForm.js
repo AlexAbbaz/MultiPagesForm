eb8b66518317bb7eec2766a643f5b6247a7ebe67
REPENTEZ_VOUS <alexabbaz0@gmail.com>
2024-06-09 21:41:41 +0300

    3 pages form with basic event handling

--- force-app/main/default/lwc/multiPagesForm/multiPagesForm.js
+++ force-app/main/default/lwc/multiPagesForm/multiPagesForm.js
@@ -1,3 +1,39 @@
 import { LightningElement } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
-export default class MultiPagesForm extends LightningElement {}
+export default class MultiPagesForm extends LightningElement {
+    contactName = ''
+    contactBirthdate = ''
+    contactCountry = ''
+    contactPhone = ''
+
+    handleName(ev){
+        this.contactName = (ev.detail.salutation || '') + (ev.detail.firstName || '') + ' ' + (ev.detail.lastName || '')
+    }
+
+    handleBirthdate(ev){
+        this.contactBirthdate = ev.target.value
+    }
+
+    handleCountry(ev){
+        this.contactCountry = ev.target.value
+    }
+
+    handlePhone(ev){
+        this.contactPhone = ev.target.value
+    }
+
+    handleSubmit(event) {
+        // event.preventDefault()
+        console.log('submit', event.detail.fields)
+    }
+
+    handleSuccess(event) {
+        const payload = event.detail;
+        const toast = new ShowToastEvent({
+            title: 'Get Help',
+            message: 'New Contact ' + payload.fields.LastName.value,
+        });
+        this.dispatchEvent(toast);
+    }
+}

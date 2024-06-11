6a74c8577c2b31be84b348485aa5ca6bf711fc72
REPENTEZ_VOUS <alexabbaz0@gmail.com>
2024-06-11 15:00:11 +0300

    progress bar and 'step' logic

--- force-app/main/default/lwc/multiPagesForm/multiPagesForm.js
+++ force-app/main/default/lwc/multiPagesForm/multiPagesForm.js
@@ -7,32 +7,50 @@ export default class MultiPagesForm extends LightningElement {
     contactCountry = ''
     contactPhone = ''
 
-    handleName(ev){
-        this.contactName = (ev.detail.salutation || '') + (ev.detail.firstName || '') + ' ' + (ev.detail.lastName || '')
+    nameExists = false
+
+    currentStep = '1'
+    isComplete = false
+
+    handleStep(ev) {
+        this.currentStep = ev.target.value
     }
 
-    handleBirthdate(ev){
+    handleName(ev) {
+        this.contactName = (ev.detail.salutation || '') + (ev.detail.firstName || '') + ' ' + (ev.detail.lastName || '')
+        if (ev.detail.lastName === '' || ev.detail.lastName === null) {
+            this.nameExists = false
+        } else {
+            this.nameExists = true
+        }
+    }
+
+    handleBirthdate(ev) {
         this.contactBirthdate = ev.target.value
     }
 
-    handleCountry(ev){
+    handleCountry(ev) {
         this.contactCountry = ev.target.value
     }
 
-    handlePhone(ev){
+    handlePhone(ev) {
         this.contactPhone = ev.target.value
     }
 
     handleSubmit(event) {
-        // event.preventDefault()
-        console.log('submit', event.detail.fields)
+        event.preventDefault();
+        const fields = event.detail.fields;
+        this.template.querySelector('lightning-record-edit-form').submit(fields);
     }
 
     handleSuccess(event) {
+        this.isComplete = true
+        this.currentStep = 'complete'
         const payload = event.detail;
         const toast = new ShowToastEvent({
             title: 'Get Help',
             message: 'New Contact ' + payload.fields.LastName.value,
+            variant: 'success'
         });
         this.dispatchEvent(toast);
     }
